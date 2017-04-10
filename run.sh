62f870062208c0cec8adc6771b4fc36dc61ce4a7
Mark Chmarny <mchmarny@mchmarny-macbookpro.roam.corp.google.com>
2017-04-10 13:29:09 -0700

    removed ghost files

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# get global vars
-. scripts/config.sh
-
-export GOOGLE_APPLICATION_CREDENTIALS="./service-account-key.json"
-
-# run
-./tfeel -q="google, gcp, bigquery, cloud" -p="${GCLOUD_PROJECT}"
