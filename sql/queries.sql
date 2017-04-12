4efd1868c2d0c0953d6acc1d9d907acf473863d0
Mark Chmarny <mchmarny@mchmarny-macbookpro.roam.corp.google.com>
2017-04-11 21:00:49 -0700

    Updared SQL query to incude twitter search query

--- sql/queries.sql
+++ sql/queries.sql
@@ -3,13 +3,13 @@
 SELECT sum(r.score) as score_sum, t.by
 FROM [mchmarny-dev:tfeel.tweets] t
 JOIN [mchmarny-dev:tfeel.results] r on t.id = r.id
-WHERE r.score < 0
+WHERE r.query='XXX' AND r.score < 0
 GROUP BY t.by
 ORDER BY 1
 
-// All tweets by user 
+// All tweets by user
 SELECT t.on, r.score, t.body
 FROM [mchmarny-dev:tfeel.tweets] t
 JOIN [mchmarny-dev:tfeel.results] r on t.id = r.id
-WHERE t.by = 'XXX'
+WHERE r.query='XXX' AND t.by = 'XXX'
 ORDER BY 1 desc
